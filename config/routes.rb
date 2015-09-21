da9c8760ab6db52044621c8c81b136385ebb21ba
katya-yani <katyayani.vyas@gmail.com>
2015-09-21 16:11:55 +0100

    Added sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  root "activity#index"
-  resources :activities
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path
+  end
+
+  root "activity#index"
+  resources :activities
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
