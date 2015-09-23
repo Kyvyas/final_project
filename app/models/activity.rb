b31df1a641e26fb3e8d013efba6302a455611fd1
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-09-23 10:25:53 +0100

    added model validations to stop negative integers on participants columns

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,8 @@ class Activity < ActiveRecord::Base
   belongs_to :user
   has_many :attendances
   has_many :attendees, through: :attendances, source: :user
+  validates_numericality_of :participants, greater_than: 0
+  validates_numericality_of :active_participants, greater_than_or_equal_to: 0
 
   def has_spaces?
     self.active_participants < self.participants
