e028ff35d4d91e934f68936429e74001ad6185ec
katya-yani <katyayani.vyas@gmail.com>
2015-09-25 09:47:44 +0100

    Added tag filtering

--- app/assets/javascripts/activity.js
+++ app/assets/javascripts/activity.js
@@ -1,18 +1,1 @@
-$(document).ready(function() {
-
- $('.search_button').on('click', function(event){
-     event.preventDefault();
-     console.log("HI")
-     var activityList = $('.activity_list');
-
-     $.get(this.href, function(response){
-       activityList.text(response.new_activity_list);
-   })
- // $('.search_button').submit(function () {
- //    $.get(this.action, $(this).serialize(), null, 'script');
- //    return false;
- //  });
- })
-})
-
 
