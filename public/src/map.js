7f7eb7104958d70005b574a02c05c3f2a8107e36
Makers Laptop 22 <makerslaptop22@Makerss-MacBook-Pro-3.local>
2015-09-25 09:44:54 +0100

    activities added to map

--- public/src/map.js
+++ public/src/map.js
@@ -1,5 +1,5 @@
 L.mapbox.accessToken = 'pk.eyJ1Ijoib3dlbmxhbWIiLCJhIjoiY2lleWljcnF4MDBiOXQ0bHR0anRvamtucSJ9.t3YnHHqvQZ8Y0MTCNy0NNw';
-var map = L.mapbox.map('map', 'mapbox.comic')
+var map = L.mapbox.map('map', 'mapbox.pirates')
     .setView([51.5072, -0.1], 9);
 $.ajax({
   dataType: 'text',
