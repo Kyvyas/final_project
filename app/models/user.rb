e2b3e3f441717985e023591bc6ddf104ff3a8bd0
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-09-26 15:14:06 +0100

    added avatars for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
 
   has_many :activities
   validates_presence_of :name
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/pirate-icon.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 
   def self.from_omniauth(auth)
--- spec/features/users_management_spec.rb
+++ spec/features/users_management_spec.rb
@@ -12,6 +12,19 @@ feature "User can sign in and out" do
       visit('/')
       expect(page).not_to have_link('Sign out')
     end
+
+    it "can sign up with a photo" do
+      visit('/')
+      click_link('Sign up')
+      fill_in('Name', with: 'Harry')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      allow_any_instance_of(Paperclip::Attachment).to receive(:url).and_return("/spec/asset_specs/photos/Pirate-Parrot.jpg")
+      click_button('Sign up')
+      click_on "My Profile"
+      expect(page).to have_css('img[src*="Pirate-Parrot.jpg"]')
+    end
   end
 
   context "user signed in on the homepage" do
@@ -48,6 +61,13 @@ feature "User can sign in and out" do
   end
 
   context 'user profile' do
+    scenario 'user can visit own profile' do
+      user = create(:user)
+      sign_in_as(user)
+      visit '/'
+      click_on 'My Profile'
+      expect(page).to have_content("Katya")
+    end
     scenario "has hosted activities" do
       user = create(:user)
       sign_in_as(user)
@@ -70,9 +90,7 @@ feature "User can sign in and out" do
       visit '/'
       click_on('Football')
       click_on("I'm in")
-      visit '/'
-      click_on('Tennis')
-      click_on('Harry')
+      click_on "My Profile"
       expect(page).to have_content("Attended activities: Football")
     end
   end
