c9ac44f9d907ca265ec63bba9f8bfe1ea3401079
Makers Laptop 22 <makerslaptop22@Makerss-MacBook-Pro-3.local>
2015-09-29 15:44:03 +0100

    working reverge geocoding

--- public/src/confirm_map.js
+++ public/src/confirm_map.js
@@ -1,29 +1,39 @@
 $(document).ready(function() {
   L.mapbox.accessToken = 'pk.eyJ1Ijoib3dlbmxhbWIiLCJhIjoiY2lleWljcnF4MDBiOXQ0bHR0anRvamtucSJ9.t3YnHHqvQZ8Y0MTCNy0NNw';
-  var confirm_map = L.mapbox.map('confirm_map', 'mapbox.streets')
-       .setView([51.5072, -0.1], 9);
+  var postcode = $('#activity_location').val();
+  $.getJSON("https://api.mapbox.com/v4/geocode/mapbox.places/"+postcode+".json?access_token=pk.eyJ1Ijoib3dlbmxhbWIiLCJhIjoiY2lleWljcnF4MDBiOXQ0bHR0anRvamtucSJ9.t3YnHHqvQZ8Y0MTCNy0NNw",function(result){
+    var result = result.features[0].center
+    var confirm_map = L.mapbox.map('confirm_map', 'mapbox.streets')
+         .setView([result[1], result[0]], 15);
 
-  var coordinates = document.getElementById('coordinates');
+         var marker = L.marker([result[1], result[0]], {
+             icon: L.mapbox.marker.icon({
+               'marker-color': '#f86767'
+             }),
+             draggable: true
+         }).addTo(confirm_map);
+         marker.on('dragend', ondragend);
 
-  var marker = L.marker([51.5072, -0.1], {
-      icon: L.mapbox.marker.icon({
-        'marker-color': '#f86767'
-      }),
-      draggable: true
-  }).addTo(confirm_map);
 
-  // every time the marker is dragged, update the coordinates container
-  marker.on('dragend', ondragend);
+         ondragend();
 
-  // Set the initial marker coordinate on load.
-  ondragend();
+         function ondragend() {
+           var m = marker.getLatLng();
+           var lat = m.lat
+           var lng = m.lng
+           console.log(lat,lng)
+           $("#activity_latitude").val(lat);
+           $("#activity_longitude").val(lng);
 
-  function ondragend() {
-    var m = marker.getLatLng();
-    coordinates.innerHTML = 'Latitude: ' + m.lat + '<br />Longitude: ' + m.lng;
-    var lat = m.lat
-    var lng = m.lng
-    $("#activity_latitude").val(lat);
-    $("#activity_longitude").val(lng);
-  }
+           $.getJSON("https://api.mapbox.com/v4/geocode/mapbox.places/"+lng+","+lat+".json?access_token=pk.eyJ1Ijoib3dlbmxhbWIiLCJhIjoiY2lleWljcnF4MDBiOXQ0bHR0anRvamtucSJ9.t3YnHHqvQZ8Y0MTCNy0NNw",function(dataResult){
+             var address = dataResult.features[0].place_name;
+             $("#activity_location").val(address);
+
+           });
+
+
+         }
+        var coordinates = document.getElementById('coordinates');
+
+    });
 });
